e5783a75b997b4f5db32d2c3b9cb4a4bece7049e
aloeffler <aloeffler.j@gmail.com>
2020-09-25 02:41:40 -0300

    Testando Scripts Movimento Player
    🌟 Testando script movimento
       🌠 Fonte: https://www.youtube.com/watch?v=dwcT-Dch0bA&ab_channel=Brackeys
    🌟 Aplicado ao prefab "Rose"
       🌠 Tentar aplicar o script de "better jump"

---
+++ game teste/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+  
+{
+    public CharacterController2D controller;
+
+    [SerializeField]
+    private float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame -> used for input
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    // Used to apply the input to the character
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
