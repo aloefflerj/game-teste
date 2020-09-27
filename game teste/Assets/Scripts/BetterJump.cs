d3bc971891448b7058ee577fb54bc637a574aff0
aloeffler <aloeffler.j@gmail.com>
2020-09-27 04:27:48 -0300

    Physics of Blocks
    Still working on it

--- game teste/Assets/Scripts/BetterJump.cs
+++ game teste/Assets/Scripts/BetterJump.cs
@@ -21,12 +21,10 @@ public class BetterJump : MonoBehaviour
         if (rigbody.velocity.y < 0)
         {
             rigbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-            Debug.Log("fallmult");
         }
         else if (rigbody.velocity.y > 0 && !Input.GetButton("Jump"))
         {
             rigbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-            Debug.Log("lowjump");
         }
     }
 }
---
+++ game teste/Assets/Scripts/GroundMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    private float tilesSize;
+    private float cameraSize;
+    private float thresHold;
+    private float width;
+    [SerializeField] Camera camera;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraSize = camera.orthographicSize;
+        Vector2 screenPosition = camera.WorldToScreenPoint(transform.position);
+
+        //foreach (Transform child in transform)
+        //{
+            width = transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.size.x;
+            Debug.Log("widht: " + width);
+            
+        //}
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log(Screen.width - transform.GetChild(0).transform.localScale.x);
+        transform.position -= new Vector3(0.1f, 0);
+
+        Vector2 screenPosition = camera.WorldToScreenPoint(transform.position);
+        if ((screenPosition.x) < -100)
+        {
+            Destroy(gameObject);
+        }
+            
+        Debug.Log("screenPosition: " + (screenPosition.x - width));
+        Debug.Log("Screen.width " + (-Screen.width));
+        Debug.Log("width " + (width));
+    }
+}
